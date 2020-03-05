02cc254ea9d43aad7c81ed414c9074372ca31597
Danbvb <61851724+Danbvb@users.noreply.github.com>
2020-03-05 20:15:43 -0300

    Add files via upload

---
+++ banho.py
@@ -0,0 +1,8 @@
+#Em um banho de 5 minutos, fechando o registro ao se ensaboar, são gastos 45 litros de água. Sabendo que em 1 m3 de água há 1.000 litros, calcule quantos banhos de 5 minutos são necessários para consumir 1 metro cúbico de água?
+
+
+minuto = int(input("Quantos minutos voce utilizou para tomar banho: "))
+litro = 9
+consumo = minuto * 9
+banho = consumo / 45
+print (" o numero de banho com o tempo de 5 minutos foi de %.2f" % banho)
