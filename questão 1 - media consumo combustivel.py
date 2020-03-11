646f4d6fd90733f7ea3bff29b79c27726e2324f5
Danbvb <61851724+Danbvb@users.noreply.github.com>
2020-03-11 19:01:18 -0300

    Delete questão 1 - media consumo combustivel.py

--- questão 1 - media consumo combustivel.py
+++
@@ -1,7 +0,0 @@
-#3. Faça um programa que calcule a média de consumo de combustível de um veículo. O usuário deve informar o KM inicial (ex. 12500 km), o KM final (ex. 12700 km) e quantos litros foram gastos no percurso.
-
-Km_inicial = int(input("Insira o valor da distancia inicial: "))
-Km_final = int(input("Insira o valor da distancia final: "))
-consumo = int(input("Quantos litros de combustivel foram gastos durante o percurso: "))
-media = ( Km_final - Km_inicial) / consumo
-print ("A media de consumo de combustivel sera de %.fKm por Lt." % media)
