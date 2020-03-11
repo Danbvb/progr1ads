5ba2e001bfcba19d976595083bea8e8e42fe3519
Danbvb <61851724+Danbvb@users.noreply.github.com>
2020-03-11 20:48:25 -0300

    Delete reajuste salario.py

--- reajuste salario.py
+++
@@ -1,6 +0,0 @@
-#10. Faça um programa que calcula o novo valor do salário de um funcionário. O usuário informa o salário atual (ex. 1250,00) e o percentual do reajuste (ex. 13,5 %).
-
-salario_funcionario = int(input("Informe seu salario atual: "))
-reajuste = salario_funcionario * 13.5 / 100
-novo_salario = salario_funcionario + reajuste
-print ("O seu novo salario sera de R$%.2f" % novo_salario)
