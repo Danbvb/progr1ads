8197347d6a875ddaa8a84f735d18a368f664f38c
Danbvb <61851724+Danbvb@users.noreply.github.com>
2020-03-11 20:48:06 -0300

    Delete cm para polegada.py

--- cm para polegada.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que converta valores de centímetros em polegadas.
-
-cm = int(input("Insira o valor desejado: "))
-polegada = cm / 2.54
-print ("O valor sera de %.3fin" % polegada)
