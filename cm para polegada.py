8fe7a6b91f91241cf6a05887f55b76c72c8109db
Danbvb <danilosouza@ads.fiponline.edu.br>
2020-03-11 20:45:25 -0300

    Segunda questão - exercício 04/03/2020

---
+++ cm para polegada.py
@@ -0,0 +1,5 @@
+#Escreva um programa que converta valores de centímetros em polegadas.
+
+cm = int(input("Insira o valor desejado: "))
+polegada = cm / 2.54
+print ("O valor sera de %.3fin" % polegada)
