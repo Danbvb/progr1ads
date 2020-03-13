7493d29b04f2735717e88d0750e5afcbf4ba3416
Danbvb <danilosouza@ads.fiponline.edu.br>
2020-03-13 16:22:29 -0300

    Exercício 04/03/2020

---
+++ Q6 - quadrado.py
@@ -0,0 +1,5 @@
+#Escreva um programa que leia um valor inteiro e calcule o seu quadrado.
+#DaniloSouza #04/03/2020
+inteiro = int(input("Insira o valor desejado: "))
+valor = inteiro ** 2
+print (valor)
---
+++ Q8 - cubo.py
@@ -0,0 +1,5 @@
+#Escreva um programa que leia um valor inteiro e calcule o seu cubo.
+#DaniloSouza #13/03/2020
+inteiro = int(input("Insira o valor desejado: "))
+valor = inteiro ** 3
+print (valor)
