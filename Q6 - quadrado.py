83c59554d68d7f481c8166e875b307c937aa9fca
Danbvb <61851724+Danbvb@users.noreply.github.com>
2020-03-14 23:59:38 -0300

    Delete Q6 - quadrado.py

--- Q6 - quadrado.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que leia um valor inteiro e calcule o seu quadrado.
-#DaniloSouza #04/03/2020
-inteiro = int(input("Insira o valor desejado: "))
-valor = inteiro ** 2
-print (valor)
