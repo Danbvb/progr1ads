5fc911b85dce9479e90289b9a982ac2d8eb41559
Danbvb <61851724+Danbvb@users.noreply.github.com>
2020-03-14 23:59:57 -0300

    Delete Q8 - cubo.py

--- Q8 - cubo.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que leia um valor inteiro e calcule o seu cubo.
-#DaniloSouza #13/03/2020
-inteiro = int(input("Insira o valor desejado: "))
-valor = inteiro ** 3
-print (valor)
